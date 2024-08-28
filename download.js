c4b356d6eda9cd0dee2d444bd73a56498b2c6bf9
k-tafakkori <ktafakkori@gmail.com>
2024-08-28 13:15:09 +0330

    revised files.

---
+++ download.js
@@ -0,0 +1,26 @@
+function detectOS() {
+    const userAgent = window.navigator.userAgent;
+    if (userAgent.indexOf('Win') !== -1) return 'Windows';
+    if (userAgent.indexOf('Mac') !== -1) return 'Mac';
+    if (userAgent.indexOf('X11') !== -1) return 'Unix';
+    if (userAgent.indexOf('Linux') !== -1) return 'Linux';
+    return 'Unknown';
+}
+
+function setDownloadOptions() {
+    const os = detectOS();
+    const downloadButton = document.getElementById('download-button');
+    const downloadInfo = document.getElementById('download-info');
+
+    switch (os) {
+        case 'Windows':
+            downloadButton.onclick = () => window.location.href = 'https://github.com/ktafakkori/feloopy/releases/download/0.3.5/feloopy-engine.exe';
+            downloadInfo.textContent = 'For Windows 11/10 64-bit.';
+            break;
+        default:
+            downloadButton.onclick = () => alert('Your OS is not supported.');
+            downloadInfo.textContent = 'Your OS is not supported.';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', setDownloadOptions);
