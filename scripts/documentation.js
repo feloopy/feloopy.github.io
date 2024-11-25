0bf7c36223b61db7beffbff29681c57020297cc2
k-tafakkori <ktafakkori@gmail.com>
2024-11-26 00:05:21 +0330

    revised files.

--- scripts/documentation.js
+++ scripts/documentation.js
@@ -1,7 +1,7 @@
 function setDocumentationButton(documentationButtonId) {
     const documentationButton = document.getElementById(documentationButtonId);
     documentationButton.onclick = () => {
-        window.location.href = 'https://github.com/feloopy/documentation/blob/main/feloopy-documentation.pdf'; // Link to the documentation website
+        window.location.href = 'https://raw.githubusercontent.com/feloopy/documentation/main/feloopy-documentation.pdf'; // Link to the documentation website
     };
 }
 
