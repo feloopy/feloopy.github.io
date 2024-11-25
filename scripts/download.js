aebb88f2dbef54e4c1d8c2787ca833bbe79f0638
k-tafakkori <ktafakkori@gmail.com>
2024-11-26 00:45:26 +0330

    revised files.

--- scripts/download.js
+++ scripts/download.js
@@ -7,20 +7,35 @@ function detectOS() {
     return 'Unknown';
 }
 
+async function getLatestReleaseDownloadLink() {
+    const response = await fetch('https://api.github.com/repos/feloopy/engine/releases/latest');
+    const data = await response.json();
+
+    const zipAsset = data.assets.find(asset => asset.name === 'feloopy-engine.zip');
+    return zipAsset ? zipAsset.browser_download_url : null;
+}
+
 function setDownloadOptions(downloadButtonId, downloadInfoId) {
     const os = detectOS();
     const downloadButton = document.getElementById(downloadButtonId);
     const downloadInfo = document.getElementById(downloadInfoId);
 
-    switch (os) {
-        case 'Windows':
-            downloadButton.onclick = () => window.location.href = 'https://github.com/feloopy/engine';
-            downloadInfo.textContent = 'For Windows 11/10 64-bit.';
-            break;
-        default:
-            downloadButton.onclick = () => alert('Please use "pip install -U feloopy" instead.');
-            downloadInfo.textContent = 'Your OS is not supported.';
-    }
+    getLatestReleaseDownloadLink().then(latestReleaseUrl => {
+        if (latestReleaseUrl) {
+            switch (os) {
+                case 'Windows':
+                    downloadButton.onclick = () => window.location.href = latestReleaseUrl;
+                    downloadInfo.textContent = 'For Windows 11/10 64-bit.';
+                    break;
+                default:
+                    downloadButton.onclick = () => alert('Please use "pip install -U feloopy" instead.');
+                    downloadInfo.textContent = 'Your OS is not supported.';
+            }
+        } else {
+            downloadButton.onclick = () => alert('Latest release not found.');
+            downloadInfo.textContent = 'Failed to fetch latest release.';
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
