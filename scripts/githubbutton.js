40d5485729acfa357924755d61a4ada365fe9d36
k-tafakkori <ktafakkori@gmail.com>
2025-01-01 12:14:45 +0330

    revised files.

--- scripts/githubbutton.js
+++ scripts/githubbutton.js
@@ -4,7 +4,7 @@ const githubIcon = document.getElementById('github-icon');
 document.getElementById('github-button1').addEventListener('mouseenter', function() {
     githubIcon.classList.remove('fab', 'fa-github');
     githubIcon.classList.add('fas', 'fa-star');
-    document.querySelector('span').textContent = 'Star this repository!';
+    document.querySelector('span').textContent = "Star FelooPy's repository!";
 });
 
 document.getElementById('github-button1').addEventListener('mouseleave', function() {
