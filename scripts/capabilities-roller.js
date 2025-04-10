d2a62801292f9aa4ca736f6482ab6383c39a630b
k-tafakkori <ktafakkori@gmail.com>
2025-04-10 16:47:53 +0330

    revised files.

--- scripts/capabilities-roller.js
+++ scripts/capabilities-roller.js
@@ -3,14 +3,14 @@ const phrases = [
   "minimize Redundancy", "maximize Employees", "minimize Employees", "maximize Personalization", "minimize Expenditure",
   "maximize Closeness", "minimize Cost", "maximize Equity", "minimize Deviation", "maximize Return on Investment", "minimize Consumption",
   "maximize Resilience", "minimize Earliness", "maximize Profit", "minimize Establishments", "maximize Sales",
-  "minimize Inventory", "maximize Net Present Value", "minimize Tardiness", "maximize Speed", "minimize Backlog", "maximize Viability",
-  "minimize Idleness", "maximize Satisfaction", "minimize Risks", "maximize Greenness", "minimize Workload",
+  "minimize Inventory", "maximize Net Present Value", "minimize Stockouts", "minimize Tardiness", "maximize Speed", "minimize Backlog", "maximize Viability", "minimize Downtime",
+  "minimize Idleness", "minimize Waste", "maximize Satisfaction", "minimize Risks", "maximize Greenness", "minimize Workload",  "maximize Performance",
   "maximize Sustainability", "minimize Transportation", "maximize Happiness", "minimize Returns", "maximize Turnover",
-  "minimize Vulnerability", "maximize Impressions", "minimize Tardy Jobs", "maximize Robustness", "minimize Shortage",
+  "minimize Vulnerability", "maximize Impressions", "minimize Tardy Jobs", "maximize Robustness", "minimize Shortage", "minimize Depreciation",
   "maximize Outputs", "minimize Dependencies", "maximize Exports", "minimize Resources", "maximize Reliability",
   "minimize Fatigue", "maximize Job Creation", "minimize Fleet Size", "maximize Social Responsibility", "minimize Inputs",
   "maximize Efficiency", "minimize Loss", "maximize Effectiveness", "minimize Imports", "maximize Quality", "minimize Cycle Time", "maximize Market Share", "minimize Makespan", "minimize Perishability", "minimize Outsourcing",
-];
+  "maximize Yield", "maximize Coverage", "minimize Maintenance Requirements", "minimize Requirements", "maximize Uptime"];
 
 const roller = document.getElementById('roller');
 let index = 0;
